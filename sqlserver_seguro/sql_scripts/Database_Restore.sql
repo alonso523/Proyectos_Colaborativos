7ff1c6d04d60a08385625591ffec1d0523b61281
alonso523 <alonso523@outlook.com>
2026-04-19 21:39:28 -0600

    Version actualizada

--- sqlserver_seguro/sql_scripts/Database_Restore.sql
+++ sqlserver_seguro/sql_scripts/Database_Restore.sql
@@ -1,3 +1,4 @@
+--cp "C:\Administracion de Bases Datos\Cuatrimestre 4\DDA\TiendaMediosDigitales_DW_SA_Proyecto_DDA.bak" sql_seguro:/var/opt/mssql/backup/TiendaMediosDigitales_DW_SA_Proyecto_DDA.bak
 RESTORE DATABASE TiendaMediosDigitales_DW_SA_Proyecto_DDA
 FROM DISK = '/var/opt/mssql/backup/TiendaMediosDigitales_DW_SA_Proyecto_DDA.bak'
 WITH MOVE 'TiendaMediosDigitales_DW_SA_Proyecto_DDA'
